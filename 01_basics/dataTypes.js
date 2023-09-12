9b9127f5efb132dd29ecc849574daa83a34b77a8
vikas8236 <100841606+vikas8236@users.noreply.github.com>
2023-09-12 09:06:12 +0000

    Variables and Datatypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+// alert(3 + 3) // we are using nodeJs not browser
+
+let name ="vikas" // String
+let age = 234;     // number
+let gender ='M'     //string
+let a = true;       //boolean
+let pi = 3.14;
+console.log(typeof pi);
+
+
+// null = empty;
+// boolean => true/false
+// undefined => which is not defined
+// symbol => unique
+// and many more
---
+++ 01_basics/variables.js
@@ -0,0 +1,5 @@
+console.log("vikas dwivedi")
+let accountHolder = 'vikasbarahiya'
+let accountNo = 434324234
+const a = 34;
+console.table([accountHolder, accountNo, a])
