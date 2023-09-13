77cab9b289ca04a4f6e017d8e61a913267241e8d
vikas8236 <100841606+vikas8236@users.noreply.github.com>
2023-09-13 08:24:28 +0000

    datatype summary

--- 01_basics/dataTypes.js
+++ 01_basics/dataTypes.js
@@ -16,3 +16,8 @@ console.log(typeof pi);
 // and many more
 /*************** Datatype Summary*********** */
 
+// there are two types of datatypes in javaScript Primitive and Non-Primitive
+// Primitive => Number, String, Null, Boolean, symbol, BigInt, Undefined
+// Non-Primitive => Array, Object, function
+// Stack memory is used for primitive datatypes and
+// Heap Memory is  used for Non-primitive datatypes
