fa01349cbeacdc406c98f870c53c4aabe38a5636
vikas8236 <100841606+vikas8236@users.noreply.github.com>
2023-09-13 07:39:15 +0000

    conversion Operations

---
+++ 01_basics/ConversionOperations.js
@@ -0,0 +1,32 @@
+let score = 434; // Number 
+console.log(typeof score) //type = number
+console.log(score)          // 434
+let stringScore = String(score)  // converting to String
+console.log(stringScore)    
+console.log(typeof stringScore)
+let yes = 0;
+let booleanTrue = Boolean(yes) //conversion into the boolean
+console.log(booleanTrue)
+
+let num = "3ar3" // string
+console.log("type of num = "+typeof num)
+let num2 = Number(num)  // converting into string
+console.log(num2) // value is changed so it is not recommended
+// undefined to number => NaN
+// boolean to number true => 1 and
+// false => 0
+// null to number => 0
+
+
+
+/* **************Operations************ */
+console.log("it's a different part of operations")
+let a = 34;
+let b = 56;
+console.log("addintion of 34 and 56 is: "+(a + b))
+let str1 = "vikas "
+let str2 = "Dwivedi"
+console.log(str1 + str2) // concatination of two string using addition operator.
+
+let val = 45;
+console.log(str1 + val) // concatination of string and number
