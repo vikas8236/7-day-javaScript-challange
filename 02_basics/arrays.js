7cb5cc5325b659c1255139ea570763e85ee701c8
vikas8236 <100841606+vikas8236@users.noreply.github.com>
2023-09-18 18:28:53 +0000

    Arrays-01

---
+++ 02_basics/arrays.js
@@ -0,0 +1,21 @@
+const arr = [0, 1, 2, 3, 4, 5]
+console.log(arr[0]);
+const  arr2 = [3, 'vikas', 'ajay', 36, 'r' ];
+let i=0;
+for( i = 0;i<arr2.length;i++){
+    console.log(arr2[i])
+}
+// Array Methods
+ arr.push(67);
+ for( i = 0;i<arr.length;i++){
+    console.log(arr[i])
+}
+arr.pop();
+for( i = 0;i<arr.length;i++){
+   console.log(arr[i])
+}
+arr.shift();
+
+for( i = 0;i<arr.length;i++){
+   console.log(arr[i])
+}
