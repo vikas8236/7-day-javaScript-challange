c42882bb81d5f710fb9253fed19ff9f3b670bbb7
vikas8236 <100841606+vikas8236@users.noreply.github.com>
2023-09-21 18:24:29 +0000

    Objects_basics01

---
+++ 02_basics/objects01.js
@@ -0,0 +1,18 @@
+// Declaration of object
+const obj = {
+    name: "vikas",
+    age: 20,
+    gender: "M",
+    Education:"bachelor's"
+}
+console.log(obj)
+// another way of Declaring Object
+let student = new Object();
+student.name = "vikas Dwivedi";
+student. age = "20";
+console.log(student);
+// Ok one more way
+console.log(obj["name"]);
+console.log(student["age"]);
+// Objects are also created using create() constructor
+
