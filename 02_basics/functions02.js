4b507a22f95ed612e23d61d12a129555abc87215
vikas8236 <100841606+vikas8236@users.noreply.github.com>
2023-09-26 18:18:32 +0000

    local and global Scope

---
+++ 02_basics/functions02.js
@@ -0,0 +1,16 @@
+//local and global scope of var and let 
+// we can define variable with three ways
+// var a , let a , const a
+// why not use var 
+var a = 90;
+let b =30;
+if(true){
+   var  a = 89;
+   let  b = 78;
+    console.log(a);
+    console.log(b);
+}
+console.log(a);
+console.log(b);
+// difference b/w var & let is depends upon scope.
+// variable defind  with  let can have differnt values in different scopes but in case of var if we reintialize the variable inside the scope and we want to access the variable at local scope the the value will change with new intialized value which is not good for a programmer
